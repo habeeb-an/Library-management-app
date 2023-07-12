cf8b8bf85fae1c7e65ac9334327ee8a645673e97
habeeb-an <hbareacode@gmail.com>
2023-07-12 18:45:17 +0530

    bugfix

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import qs from 'qs'
-const baseURL= process.env.REACT_APP_API_ENDPOINT
+const baseURL= import.meta.process.env.REACT_APP_API_ENDPOINT
 const axiosInstance = axios.create({
   baseURL: baseURL,
   // Set your API base URL
