5cf26d0945ccdb2f18bdd99142da9404a98a858d
habeeb-an <hbareacode@gmail.com>
2023-07-12 17:58:58 +0530

    cors error client fix

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import qs from 'qs'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/', // Set your API base URL
+  baseURL: process.env.REACT_APP_API_ENDPOINT,
+  // Set your API base URL
   timeout: 5000, // Set a default timeout (optional)
   headers: {
     'Content-Type': 'application/json', // Set default headers (optional)
